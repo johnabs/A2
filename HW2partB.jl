9cedac3b00545730e2128dc312982a8c6e19b65e
theopurdue <92651266+theopurdue@users.noreply.github.com>
2021-11-01 13:12:50 -0400

    Fully functional version
    Missing appropriate commenting, which will be added shortly

--- HW2partB.jl
+++ HW2partB.jl
@@ -1,6 +1,4 @@
-using Evolutionary
-using CSV
-using DataFrames
+using Evolutionary, CSV, DataFrames, StatsPlots
 
 function getInstance(file)
     data=CSV.read(file,DataFrame,skipto=6,header=false)
@@ -21,48 +19,99 @@ end
 
 function bestPartA(con,n,w,v,popSize)
     
-    mutRate = 0.9 #UPDATE LATER
-    crossRate = 0.9 #UPDATE LATER
+    function x0(n)
+        return flip(BitVector(zeros(n)))
+    end
     
-    #Create objective function, which defaults to 0 if it violates the constraint,
+    mutRate = (8/9)*((n-1)/(popSize*n)) #From part a
+    crossRate = 0.5 #From part a
+    
+    #Create objective function, which defaults to a dynamic penalty if it violates the constraint,
     #and the sum is set to negative as this EA uses minimization
-    f(x)=((sum(w.*x)<=con) ? -sum(v.*x) : sum(w.*x))
-
-    #Create bitvector to represent the selected items; provide at least 1 zero, so the objective function will actually return a good score
-    #starting out; otherwise it just prematurely converges to 0.
-    x0 = BitVector(vcat(zeros(n-1),1)) 
-
-    ga = GA(populationSize=popSize,selection=roulette,mutation=flip,crossover=uniform,mutationRate=mutRate,crossoverRate=crossRate,ɛ = 0.1);
-    gaResults = Evolutionary.optimize(f, x0, ga, Evolutionary.Options(iterations=10000))
+    f(x)=((sum(w.*x)<=con) ? -sum(v.*x) : (sum(w.*x)-con))
+    
+    ga = GA(populationSize=popSize,selection=roulette,mutation=flip,crossover=singlepoint,mutationRate=mutRate,crossoverRate=crossRate,ɛ = 0.1);
+    gaResults = Evolutionary.optimize(f, x0(n), ga, Evolutionary.Options(iterations=10000))
     
     return gaResults #technically unecessary, but helps me think
 end
 
 function partb(file)
     con,n,w,v = getInstance(file)
-    popSize = 1000 #might not need to vary population size? TBD
-    gaResults = bestPartA(con,n,w,v,popSize)
+    popSize = [n,3*n,5*n,7*n,9*n,11*n,13*n]
     f(x) = -sum(v.*x)
-    
-    function randSampling()
-        function repair(x)
-            while sum(w.*x) > con
-                x[rand(findall(x))] = false
+    runs = 30
+
+    function feasibleRand(currentGA)
+        function oneSol(x)
+            weight = sum(w.*x)
+            while weight < con
+                item = rand(findall(x->x==0,x))
+                x[item] = true
+                if sum(w.*x) > con
+                    x[item] = false
+                    weight = con + 1
+                else
+                    weight = sum(w.*x)
+                end
             end
             return x
         end
-
-        numSolns = Evolutionary.f_calls(gaResults)
-        solutions = repair.([rand(Bool,n) for _ in 1:numSolns])
+        numSolns = Evolutionary.f_calls(currentGA)
+        solutions = oneSol.([BitVector(zeros(n)) for _ in 1:numSolns])
         bestFitness = minimum(f.(solutions))
         return bestFitness
     end
+    
+    function truRand(currentGA)
+        numSolns = Evolutionary.f_calls(currentGA)
+        solutions = [BitVector(rand([0 1],n)) for _ in 1:numSolns]
+        weights = [sum(w.*x) for x in solutions]
+        infeasible = findall(weights -> weights > con, weights)
+        fitness = f.(solutions)
+        fitness[infeasible] .= 0
+        bestFitness = minimum(fitness)
+        return bestFitness
+    end
+    
+    gaBests = zeros(runs, length(popSize))
+    frandBests = zeros(runs, length(popSize))
+    trandBests = zeros(runs,length(popSize))
+    z = 1
+    for i in popSize  
+        for j in 1:runs
+            currentGA = bestPartA(con,n,w,v,i)
+            gaBests[j,z] = -Evolutionary.minimum(currentGA)
+            frandBests[j,z] = -feasibleRand(currentGA)
+            trandBests[j,z] = -truRand(currentGA)
+        end
+        z +=1
+    end
+    
+    ga_df = DataFrame(alg = fill("GA (Best Part A)",runs*length(popSize)), Npop = repeat(popSize,inner=runs), fitness = gaBests[:])
+    frand_df = DataFrame(alg = fill("RS (Feasible-only)",runs*length(popSize)), Npop = repeat(popSize,inner=runs), fitness = frandBests[:])
+    trand_df = DataFrame(alg = fill("RS (All)",runs*length(popSize)), Npop = repeat(popSize,inner=runs), fitness = trandBests[:])
+    df = vcat(ga_df,frand_df,trand_df)
 
-    randBestFitness = -randSampling()
-    gaBestFitness = -Evolutionary.minimum(gaResults)
-    return gaBestFitness, randBestFitness
+    return df
 end
 
-#Set Filename and Read CSV into DataFrame
-filename="knapPI_16_50_1000.csv"
-partb(filename)
+filename = ["knapPI_11_100_1000.csv", "knapPI_13_50_1000.csv",
+"knapPI_13_200_1000.csv","knapPI_14_50_1000.csv",
+"knapPI_15_50_1000.csv","knapPI_16_50_1000.csv"]
+
+dfOne,dfTwo,dfThree,dfFour,dfFive,dfSix = partb.(filename)
+
+plotA = @df dfOne groupedboxplot(string.(:Npop), :fitness, group = :alg, title="Box Plot of Fitness vs Population Size",legend=:outerright)
+plotB = @df dfTwo groupedboxplot(string.(:Npop), :fitness, group = :alg, title="Box Plot of Fitness vs Population Size",legend=:outerright)
+plotC = @df dfThree groupedboxplot(string.(:Npop), :fitness, group = :alg, title="Box Plot of Fitness vs Population Size",legend=:outerright)
+plotD = @df dfFour groupedboxplot(string.(:Npop), :fitness, group = :alg, title="Box Plot of Fitness vs Population Size",legend=:outerright)
+plotE = @df dfFive groupedboxplot(string.(:Npop), :fitness, group = :alg, title="Box Plot of Fitness vs Population Size",legend=:outerright)
+plotF = @df dfSix groupedboxplot(string.(:Npop), :fitness, group = :alg, title="Box Plot of Fitness vs Population Size",legend=:outerright)
+
+png(plotA,"GAvsRand_11_100")
+png(plotB,"GAvsRand_13_50")
+png(plotC,"GAvsRand_13_200")
+png(plotD,"GAvsRand_14_50")
+png(plotE,"GAvsRand_15_50")
+png(plotF,"GAvsRand_16_50")
